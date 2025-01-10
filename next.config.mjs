47825c7e67d6867be10b07832401d1d1749fd16c
Jerry <brownjer3@gmail.com>
2025-01-10 12:55:50 -0600

    fix attempt 4

--- next.config.mjs
+++ next.config.mjs
@@ -10,14 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.ts$/,
-      use: 'ts-loader',
-      exclude: /node_modules/,
-    });
-    return config;
-  },
 }
 
 export default nextConfig;
