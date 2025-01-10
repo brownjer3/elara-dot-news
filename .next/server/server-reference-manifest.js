7e90b96feaefd8d54754cc372be820eba924f85e
Jerry <brownjer3@gmail.com>
2025-01-10 12:53:32 -0600

    fix attempt 3

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"cUysnjL9LfigG9CivxhqP7otWicrxc9JOzNrl/fIzF0=\"}"
